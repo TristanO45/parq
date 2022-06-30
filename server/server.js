77e081e4223ff5f69cc4c256e8161ecf8a9ba09c
MOakes7 <mike.oakes77@gmail.com>
2022-06-29 19:56:35 -0500

    initial commit 7:55pm CST 29 Jun 2022

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+//app.use(express.urlencoded());
+
+
+
+
+
+app.listen(PORT, ()=>{ console.log(`Listening on port ${PORT}...`); });
+
+module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+    //   {
+    //     test: /\.(js|jsx)$/,
+    //     exclude: /node_modules/,
+    //     use: {
+    //       loader: 'babel-loader',
+    //       options: {
+    //         presets: ['@babel/preset-env', '@babel/preset-react']
+    //       }
+    //     }
+    //   },
+    //   {
+    //     test: /\.(css|scss)$/,
+    //     exclude: /(node_modules|bower_components)/,
+    //     use: [
+    //       'style-loader',
+    //       'css-loader'
+    //     ],
+    //   }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './client/index.html',
+  })],
+  devServer: {
+    port: 8080,
+    static: {
+      directory: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+    },
+    open: true,
+    hot: true,
+    liveReload: true
+  },
+};
