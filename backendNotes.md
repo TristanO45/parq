da5d7f372b8236025218b365bd37844b330ee5e5
Your Name <tonfroy90@gmail.com>
2022-06-29 20:08:47 -0700

    6/29 EOC Commit

---
+++ backendNotes.md
@@ -0,0 +1,18 @@
+Wednesday June 29th //
+
+Task:
+
+1. Come up with Database design
+   - Need a User Schema (Unique username, password, email)
+     - Location will be used for both Host and Client
+   - Need a Booking Schema (Client (and/or Host?, Date of booking, location), password, email)
+   - Need a Location Schema (Address, Unique Username, price, parking options)
+
+2. API for map and how to use it.
+   - Render large map and place marker for each location in DB (homepage)
+   - Render smaller map with selected location (stretch goal)
+   - Clicking on marker should display location info
+
+3. Setup the Database using atlas (done)
+4. Connect to MongoDB(mongoose) (done)
+5. Set up server to test db users
--- server/server.js
+++ server/server.js
@@ -1,17 +1,48 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-
+const dotenv = require("dotenv");
+const app = express();
 const PORT = 3000;
 
-const app = express();
-
+dotenv.config();
 app.use(express.json());
-//app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
+// Mongo Connection
+mongoose
+  .connect(process.env.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to Mongo!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to Mongo", err);
+  });
 
+/** 
+require routers here:
+define route handlers here:
+*/
 
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => {
+    return res.status(404).send("Unknown route");
+  });
 
+// Global error handler
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: "Express error handler caught unknown middleware error",
+      status: 400,
+      message: { err: "An error occurred" },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
 
 app.listen(PORT, ()=>{ console.log(`Listening on port ${PORT}...`); });
 
